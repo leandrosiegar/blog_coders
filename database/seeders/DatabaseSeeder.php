94195c3f424c3a34c74c9e1de9d6e1453d5d9889
unknown <leandrosiegar@gmail.com>
2021-02-01 13:28:17 +0100

    crear todas las migraciones, seedes y factorys

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+
+use Illuminate\Support\Facades\Storage;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,6 +19,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        // crear la carpeta posts pq por defecto no está creada
+        Storage::deleteDirectory('posts');
+        Storage::makeDirectory('posts'); // te la crea en public\storage
+
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        Post::factory(100)->create();
+        $this->call(PostSeeder::class);
     }
 }
